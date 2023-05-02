4452faafcf11798032d6d05225d1dbb388f7bce2
Joshua Nakatani <nakatani.joshua@student.greenriver.edu>
2023-05-02 13:48:08 -0400

    Add order processing

--- index.php
+++ index.php
@@ -58,29 +58,64 @@ $f3->route('GET /dinner', function() {
     echo $view->render('views/menus/dinner.html');
 });
 
+
+// Define the happy hour route
+$f3->route('GET /happy-hour', function() {
+
+    //echo '<h1>Happy Hour</h1>';
+
+    // Display a view page
+    $view = new Template();
+    echo $view->render('views/menus/happyHour.html');
+});
+
 // Define the "/order1" -> orderForm1.html
 $f3->route('GET|POST /order1', function($f3) {
 
     // if the form has been posted
-    if ($_SERVER['REQUEST_METHOD'] == "POST");
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
         // Get the data
+        // ["food"]=> string(7) "waffles" ["meal"]=> string(9) "breakfast"
+        var_dump($_POST);
+        $food = $_POST['food'];
+        $meal = $_POST['meal'];
+        //echo ("Food: $food, Meal: $meal");
 
         // Validate the data
 
         // Store the data in the session array
+        $f3->set('SESSION.food', $food);
+        $f3->set('SESSION.meal', $meal);
+        // $_SESSION['food'] = $food;
 
         // Redirect to order2 route
         $f3->reroute('order2');
+    }
 
     // Display a view page
     $view = new Template();
     echo $view->render('views/orderForm1.html');
 });
 
-// Define the "/order2" -> orderForm1.html
-$f3->route('GET /order2', function() {
+// Define the "/order1" -> orderForm1.html
+$f3->route('GET|POST /order2', function($f3) {
 
+    // if the form has been posted
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+        // get the data
+        //var_dump($_POST);
+        // ["conds"]=> array(1) { [0]=> string(7) "ketchup"
+        $conds = implode(", ", $_POST['conds']);
+        //echo $conds;
+
+        // store the data in the session array
+        $f3->set('SESSION.condiments', $conds);
+
+        // redirect to the summary route
+        $f3->reroute('summary');
+    }
     // Display a view page
     $view = new Template();
     echo $view->render('views/orderForm2.html');
@@ -89,11 +124,13 @@ $f3->route('GET /order2', function() {
 // Define the summary route
 $f3->route('GET /summary', function() {
 
-    //echo '<h1>Order Form 2</h1>';
+    //echo '<h1>Summary</h1>';
 
     // Display a view page
     $view = new Template();
     echo $view->render('views/summary.html');
+
+    session_destroy();
 });
 
 // Run Fat-Free
